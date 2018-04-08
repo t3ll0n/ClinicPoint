fd5d3f2e3006a58741402d775f2c19e7eb364b00
t3ll0n <tellonsmith@gmail.com>
2018-04-08 01:06:00 -0500

    Add project files.

---
+++ ClinicPoint/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClinicPoint.MvcApplication" Language="C#" %>
---
+++ ClinicPoint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinicPoint.Startup))]
+namespace ClinicPoint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
