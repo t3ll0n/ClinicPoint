bc27e8c8edcb828a480f3c645b57c85831c818bb
t3ll0n <tellonsmith@gmail.com>
2018-04-11 16:35:31 -0500

    implemented other api methods for patients

--- ClinicPoint/Controllers/Api/PatientsController.cs
+++ ClinicPoint/Controllers/Api/PatientsController.cs
@@ -34,5 +34,66 @@ namespace ClinicPoint.Controllers.Api
 
             return Ok(patientDto);
         }
+
+        // GET api/patients/1
+        public IHttpActionResult GetPatient(int id)
+        {
+            var patient = _context.Patients.SingleOrDefault(p => p.Id == id);
+
+            if (patient == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Patient, PatientDto>(patient));
+        }
+
+        // POST api/patients
+        [HttpPost]
+        public IHttpActionResult CreatePatient(PatientDto patientDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var patient = Mapper.Map<PatientDto, Patient>(patientDto);
+            _context.Patients.Add(patient);
+            _context.SaveChanges();
+
+            patientDto.Id = patient.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + patient.Id), patientDto);
+        }
+
+        // PUT api/patients/1
+        [HttpPut]
+        public IHttpActionResult UpdatePatient(int id, PatientDto patientDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var patientInDb = _context.Patients.SingleOrDefault(p => p.Id == id);
+
+            if (patientInDb == null)
+                return NotFound();
+
+            Mapper.Map(patientDto, patientInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE api/patients/1
+        [HttpDelete]
+        public IHttpActionResult DeletePatient(int id)
+        {
+            var patientInDb = _context.Patients.SingleOrDefault(p => p.Id == id);
+
+            if (patientInDb == null)
+                return NotFound();
+
+            _context.Patients.Remove(patientInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
